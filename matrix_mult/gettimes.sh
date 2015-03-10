5a6c092b94c4b487d05bb0b88fb2a9451fd8f493
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-03-10 12:17:40 -0500

    Plotter

---
+++ matrix_mult/gettimes.sh
@@ -0,0 +1,4 @@
+sort -V times > timesorted
+awk '{print $1}' < timesorted > sizes
+awk '{print $2}' < timesorted > serial
+awk '{print $3}' < timesorted > parallel
--- matrix_mult/plotter.py
+++ matrix_mult/plotter.py
@@ -5,9 +5,9 @@ from numpy import *
 def m_load(fname) :
     return fromfile(fname, sep='\n')
 
-x  = m_load('x')
-y1 = m_load('y1')
-y2 = m_load('y2')
+x  = m_load('sizes')
+y1 = m_load('parallel')
+y2 = m_load('serial')
 plt.plot(x, y1)
 plt.plot(x, y2)
 
