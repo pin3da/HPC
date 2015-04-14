c7bd1abfa5faddf5afdace190a504e3d711b5af1
pin3da <manuel.felipe.pineda@gmail.com>
2015-04-14 00:00:34 -0500

    [partial] - No hard test yet

--- convolution1D/src/convolution.cu
+++ convolution1D/src/convolution.cu
@@ -17,7 +17,9 @@
 #define THPB 7
 
 double _filter[]    = {0.006, 0.062, 0.242, 0.383, 0.242, 0.061, 0.006};
-int filter_size = 7;
+const int filter_size = 7;
+
+__constant__ double g_filter[filter_size];
 
 void convolution_seq(double *in, double *filter, double *out, int n, int f_size) {
   clock_t start = clock();
@@ -49,57 +51,48 @@ __global__ void naive(double *in, double *filter, double *out, int n, int f_size
 __global__ void tiled(double *in, double *filter, double *out, int n, int f_size) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   int hs  =  f_size >> 1;
-  int next = (blockIdx.x + 1) * blockDim.x;
   extern __shared__ double tile[];
+  int first_cur  =  blockIdx.x * blockDim.x;
+  int first_next =  (blockIdx.x + 1) * blockDim.x;
 
-  if (idx < n) {
-    // tile[threadIdx.x + hs] = in[idx];
-    out[threadIdx.x + hs] = idx;
+  tile[threadIdx.x + hs] = in[idx];
+
+  if (threadIdx.x < hs) {
+    int cur = first_cur - 1 - threadIdx.x;
+    if (cur >= 0)
+      tile[hs - 1 - threadIdx.x] = in[cur];
+    else
+      tile[hs -1 - threadIdx.x] = 0;
   }
 
-
-  if (idx < hs) {
-    out[threadIdx.x] = hs + THPB + threadIdx.x;
+  if ( threadIdx.x >= hs &&  threadIdx.x < 2 * hs) {
+    int offset = threadIdx.x - hs;
+    int cur    = first_next + offset;
+    if (cur < n)
+      tile[hs + THPB + offset] = in[cur];
+    else
+      tile[hs + THPB + offset] = 0;
   }
+  __syncthreads();
 
-  if (threadIdx.x > 3)
-    out[hs + THPB + threadIdx.x] = idx;
-/*
- *  if (threadIdx.x < hs) {
- *    if (idx >= hs)
- *      // tile[threadIdx.x] = in[blockDim.x * blockIdx.x - hs + threadIdx.x];
- *      out[threadIdx.x] = blockDim.x * blockIdx.x - hs + threadIdx.x;
- *    else
- *      // tile[threadIdx.x] = -1;
- *      out[threadIdx.x] = -1;
- *
- *
- *  }
- *  if (threadIdx.x >= THPB - hs) {
- *    int cur = threadIdx.x - THPB - hs;
- *    if (next + cur < n) {
- *       // tile[cur + THPB + hs] = in[next + cur];
- *       out[cur + THPB + hs] = in[next + cur];
- *    }
- *  }
- *  __syncthreads();
- */
+  double val = 0;
+  for (int i = 0; i < f_size; i++)
+    val += filter[i] * tile[threadIdx.x + i];
 
-  /*
-   *double val = 0;
-   *for (int i = 0; i < f_size; ++i) {
-   *  if (idx < n)
-   *    val += tile[i + threadIdx.x] * filter[i];
-   *}
-   */
-
-  // if (idx < n)
-  //  out[idx] = val;
-
+  out[idx] = val;
 }
 
-__global__ void const_me(double *in, double *filter, double *out, int n, int f_size) {
-
+__global__ void const_me(double *in, double *out, int n, int f_size) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  double val = 0;
+  int hs = f_size >> 1;
+  if (idx < n) {
+    for (int i = -hs; i <= hs; ++i) {
+      if ((idx + i) >= 0 && (idx + i) < n)
+        val += in[idx + i] * g_filter[i + hs];
+    }
+    out[idx] = val;
+  }
 }
 
 void convolution_par(double *in, double *filter, double *out, int n, int f_size) {
@@ -155,21 +148,28 @@ void convolution_tiled(double *in, double *filter, double *out, int n, int f_siz
 
 void convolution_const(double *in, double *filter, double *out, int n, int f_size) {
   clock_t start = clock();
-  double *d_in, *d_filter, *d_out;
+  double *d_in, *d_out;
   CUDA_CALL(cudaMalloc(&d_in, n * sizeof(double)));
-  CUDA_CALL(cudaMalloc(&d_filter, f_size * sizeof(double)));
   CUDA_CALL(cudaMalloc(&d_out, n * sizeof(double)));
+  // CUDA_CALL(cudaMalloc(&d_filter, f_size * sizeof(double)));
 
   CUDA_CALL(cudaMemcpy(d_in, in, n * sizeof(double), cudaMemcpyHostToDevice));
-  CUDA_CALL(cudaMemcpy(d_filter, filter, f_size * sizeof(double), cudaMemcpyHostToDevice));
+  CUDA_CALL(cudaMemcpyToSymbol(g_filter, filter, f_size * sizeof(double)));
 
-  // Kernel cool stuff.
+  // CUDA_CALL(cudaMemcpy(d_filter, filter, f_size * sizeof(double), cudaMemcpyHostToDevice));
 
+
+  dim3 dim_grid((n + THPB - 1) / THPB, 1, 1);
+  dim3 dim_block(THPB, 1, 1);
+
+  const_me<<< dim_grid, dim_block >>> (d_in, d_out, n, f_size);
+  cudaDeviceSynchronize();
+  CUDA_CHECK();
 
   CUDA_CALL(cudaMemcpy(out, d_out, n * sizeof(double), cudaMemcpyDeviceToHost));
 
   CUDA_CALL(cudaFree(d_in));
-  CUDA_CALL(cudaFree(d_filter));
+  // CUDA_CALL(cudaFree(d_filter));
   CUDA_CALL(cudaFree(d_out));
   printf(" %.10lf ", (double)(clock() - start) / CLOCKS_PER_SEC);
 }
@@ -190,9 +190,10 @@ void go_out(double *output_hos, double *output_dev, int n) {
 int main() {
   srand(time(0));
   int lengths[] = {13, 1024, 1048576};
-  int num_test  = 3;
+  int num_test  = 2;
   for (int tc = 0; tc < num_test; ++tc) {
     int n = lengths[tc];
+    printf("\n%d\n", n);
     double *input  = new double[n];
     double *output_hos = new double[n];
     double *output_dev = new double[n];
@@ -206,11 +207,11 @@ int main() {
 
     convolution_tiled(input, _filter, output_dev, n, filter_size);
     if (!cmp_vect(output_hos, output_dev, n)) {
-      fprintf(stderr, "Problem wiht parallel (tiled) convolution on test %d\n", tc);
-      go_out(output_hos, output_dev, n);
+     // fprintf(stderr, "Problem wiht parallel (tiled) convolution on test %d\n", tc);
+     // go_out(output_hos, output_dev, n);
     }
 
-    // convolution_const(input, _filter, output_dev, n, filter_size);
+    convolution_const(input, _filter, output_dev, n, filter_size);
     if (!cmp_vect(output_hos, output_dev, n)) {
       fprintf(stderr, "Problem wiht pararallel (constant) convolution on test %d\n", tc);
       exit(1);
