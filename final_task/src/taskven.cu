19ea2e70a4d937b9364c9a461df2f5b693df0884
pin3da <manuel.felipe.pineda@gmail.com>
2015-05-29 16:11:55 -0500

    Using czmq instead of zmqpp

--- final_task/src/taskven.cu
+++ final_task/src/taskven.cu
@@ -1,7 +1,16 @@
 #include <czmq.h>
+#include <stdlib.h>
 
-int main(int arg, char **argv) {
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("Usage %s endpoint\n", argv[0]);
+    exit(0);
+  }
 
+  puts(argv[1]);
+  zsock_t *push = zsock_new_push(argv[1]);
+
+  zsock_destroy(&push);
   return 0;
 }
 
